9e4ea72bc142920341a8cc3723965fd74eba0a15
Impstar <magesbe@gmail.com>
2019-03-11 15:00:06 -0500

    added triangle functions but didn't get to test them

---
+++ TriangleLib/TriangleLib.Tests/Triangle Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TriangleLib.Tests
+{
+    //[Theory]
+    //public void Test_Triangle_Area
+    //{
+    //}
+}
--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -1,8 +1,41 @@
 ﻿using System;
 
+
 namespace TriangleLib
 {
     public class Triangle
     {
+
+        double TriangleArea(double TriBase, double height)
+        {
+            return (TriBase * height) / 2;
+        }
+
+        double CircumRadiusTri(double a, double b, double c, double area)
+        {
+            return (a * b * c) / Math.Sqrt((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c));
+        }
+
+        bool IsEquilateral(double a, double b, double c)
+        {
+            return (a == b) && (b == c);
+        }
+
+        bool IsIsosceles(double a, double b, double c)
+        {
+            return (a == b) && (b != c) || (a == c) && (b != c) || (b == c) || (a != c);
+        }
+
+        bool IsScalene(double a, double b, double c)
+        {
+            return (a != b) && (b != c) && (a != c);
+        }
+
+        double FindThirdSide(double a, double b, double angleAB)
+        {
+            double c = 0;
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2) + 2 * a * b * Math.Cos(angleAB));
+            return c;
+        }
     }
 }
