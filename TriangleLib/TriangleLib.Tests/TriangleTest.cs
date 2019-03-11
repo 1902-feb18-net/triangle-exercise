87882d52f3e3ceb55fa1b73b4c08f69a523691a8
Andy Ly <andywindsong@gmail.com>
2019-03-11 14:59:13 -0500

    some triangle methods

---
+++ TriangleLib/TriangleLib.Tests/TriangleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TriangleLib.Tests
+{
+    public class TriangleTest
+    {
+        [Fact]
+        public void FindRightTrianglePerimeter ()
+        {
+            // arrange
+            double side1 = 3;
+            double side2 = 4;
+
+            // act
+
+        }
+    }
+}
--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -4,5 +4,50 @@ namespace TriangleLib
 {
     public class Triangle
     {
+        public static double Rad(double deg)
+        {
+            return deg * Math.PI / 180;
+        }
+
+        // finding 3rd side given 2 sides and an angle in between
+        public static double Thirdside(double side1, double side2, double angleDeg)
+        {
+            double angleRad = Rad(angleDeg);
+            return Math.Sqrt(side1 * side1 + side2 * side2 - 2 * side1 * side2 * Math.Cos(angleRad));
+        }
+
+        //public static bool SssCongruent(double side1, double side2, double side3)
+        //{
+        //    if(side1 == side2 && side2 == side3)
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
+
+        public static double RightTrianglePerimeter (double side1, double side2)
+        {
+            double side3 = PythagoreanTheorem(side1, side2);
+            return side1 + side2 + side3;
+        }
+
+        public static double PythagoreanTheorem(double side1, double side2)
+        {
+            // a^2 + b^2 = c^2, so solve for c
+            return Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2));
+        }
+
+        public static double SASPerimeter (double side1, double side2, double deg)
+        {
+            // formula is a^2 = b^2 + c^2 - 2bcCos(A)
+            double side3 = Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2) - 2 * side1 * side2 * Math.Cos(deg));
+            return side1 + side2 + side3;
+        }
+
+        public static double TriangleArea (double b, double h)
+        {
+            // A = 0.5 * b * h
+            return 0.5 * b * h;
+        }
     }
 }
