fd525e479acade08c85b1af973fd2608cd3a8b01
Matthew Valencia <MatthewLuisValencia@gmail.com>
2019-03-11 15:00:05 -0500

    Added triangle type booleans and perimeter calculation.

--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -4,5 +4,44 @@ namespace TriangleLib
 {
     public class Triangle
     {
+        //three sides
+        public int SideA { get; set; }
+        public int SideB { get; set; }
+        public int SideC { get; set; }
+        //need to modify at least one side if setting another!
+       
+        //three angles?
+        //AngleAB
+        //AngleBC
+        //AngleCA
+
+        public bool IsEquilateral()
+        {
+            return SideA == SideB && SideB == SideC;
+        }
+
+        public bool IsIsosceles()
+        {
+            return SideA == SideB || SideB == SideC || SideC == SideA;
+        }
+
+        public bool IsScalene()
+        {
+            return SideA != SideB && SideB != SideC && SideC != SideA;
+        }
+
+        public bool IsRightAngle()
+        {
+            return (SideC * SideC) == ((SideA * SideA) + (SideB * SideB) )
+                || (SideA * SideA) == ((SideC * SideC) + (SideB * SideB) )
+                || (SideB * SideB) == ((SideA * SideA) + (SideC * SideC) )  ;
+        }
+
+        public int CalculatePerimeter()
+        { return SideA + SideB + SideC; }
+
+        //Area = b*h/2
+
+
     }
 }
