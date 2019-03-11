c7f3c1612b5d2777cfe6d174df82bc30af5195f6
Daniel Rendon <danielrendon93@gmail.com>
2019-03-11 15:00:16 -0500

    initial commit

--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -4,5 +4,30 @@ namespace TriangleLib
 {
     public class Triangle
     {
+        //decimal Base { get; set; }
+        decimal Altitude { get; set; }
+        decimal[] Sides { get; set; }
+
+        public Triangle(decimal[] sides)
+        {
+            sides = Sides;
+        }
+
+
+        decimal getPerimeter(Triangle triangle)
+        {
+            decimal perimeter = 0;
+
+            for (int i = 0; i < triangle.Sides.Length; i++)
+            {
+                perimeter += triangle.Sides[i];
+            }
+            return perimeter;
+        }
+
+        decimal getArea(Triangle triangle)
+        {  
+            return triangle.Altitude * triangle.Sides[0] / 2;
+        }
     }
 }
