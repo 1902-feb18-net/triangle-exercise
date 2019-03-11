ecb68e9098d701b193c4bbafe498b6331ab7ed06
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-03-11 15:09:11 -0500

    add dummy triangle impl

--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -4,5 +4,16 @@ namespace TriangleLib
 {
     public class Triangle
     {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public double Area
+        {
+            get
+            {
+                return 1;
+            }
+        }
     }
 }
