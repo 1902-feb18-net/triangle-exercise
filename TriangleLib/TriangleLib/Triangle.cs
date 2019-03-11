8f8aab936de4bfe3a401144d0229eb4a5e7152f3
stormrubio3000 <storm-3000@hotmail.com>
2019-03-11 14:37:53 -0500

    Added properties and basic constructors.

--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -4,5 +4,26 @@ namespace TriangleLib
 {
     public class Triangle
     {
-    }
+		int Aside;
+		int Bside;
+		int Cside;
+
+		double Aangle;
+		double Bangle;
+		double Cangle;
+
+		public Triangle(double aangle, double bangle, double cangle)
+		{
+			Aangle = aangle;
+			Bangle = bangle;
+			Cangle = cangle;
+		}
+
+		public Triangle(int aside, int bside, int cside)
+		{
+			Aside = aside;
+			Bside = bside;
+			Cside = cside;
+		}
+	}
 }
