d51133ce70553282d68a71902389548f81b6689e
esqueda6021 <o_e10@outlook.com>
2019-03-11 15:29:43 -0500

    methods

--- TriangleLib/TriangleLib/Triangle.cs
+++ TriangleLib/TriangleLib/Triangle.cs
@@ -2,7 +2,61 @@
 
 namespace TriangleLib
 {
+
     public class Triangle
     {
+        int Length { get; set; }
+        int Height { get; set; }
+        int side3 { get; set; }
+
+        public double Showarea(int length, int Height)
+        {
+            return (length* Height) / 2;
+        }
+
+        public double ShowPerimeter(int Length, int Height, int side3)
+        {
+            return Length + Height + side3;
+        }
+
+
+        public bool IsEqualiteral(int Length, int Height,int side3)
+        {
+            if(Length == Height & Height == side3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool IsIsosceles(int Length, int Height, int side3)
+        {
+            if (Length == Height || Length == side3)
+            {
+                return true;
+            }
+            else
+            {
+               return false;
+            }
+        }
+
+        public bool IsScalene(int Length, int Height, int side3)
+        {
+            if (Length != Height)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
+ 
 }
